2ff82b5c84a9abf392bf96902cd36c199c370e90
violetbaggins <paolaescudero@gmail.com>
2019-12-07 10:02:13 -0300

    MP responsive home faltan alinear items

---
+++ baseAppHtml-master/controllers/mainController.js
@@ -0,0 +1,18 @@
+const fs = require('fs');
+const path = require('path');
+
+// ************ Function to Read an HTML File ************
+function readHTML (fileName) {
+	let filePath = path.join(__dirname, `/../views/${fileName}.html`);
+	let htmlFile = fs.readFileSync(filePath, 'utf-8');
+	return htmlFile;
+}
+
+const controller = {
+	root: (req, res) => {
+		let html = readHTML('index');
+		res.send(html);
+	},
+};
+
+module.exports = controller
